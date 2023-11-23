bb2d2f0f54c8dc3b59a4ebb796b71ee422004d60
raulgp <raul.garrigos@hotmail.com>
2023-11-23 17:52:05 +0100

    Iteraciones 0 - 3 done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,52 @@
-function HomePage() {}
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  const [countriesList, setCountriesList] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        "https://ih-countries-api.herokuapp.com/countries"
+      );
+      console.log(response);
+
+      setCountriesList(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (isLoading === true) {
+    return <h3>Buscando...</h3>;
+  }
+
+  return (
+    <div>
+      <h1 style={{ fontSize: "24px" }}>
+        WikiCountries: Your Guide to the World
+      </h1>
+
+      <div>
+        {countriesList.map((eachCountry) => {
+          return (
+            <p key={eachCountry._id}>
+              <Link to={`/${eachCountry.alpha3Code}`}>
+                {eachCountry.name.common}
+              </Link>
+            </p>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
 
 export default HomePage;
